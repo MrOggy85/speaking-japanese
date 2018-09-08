bd3068301f1a2811e3352b012caf953126099137
Oskar Lindgren <lindgren.oskar@gmail.com>
2018-09-08 22:41:20 +0900

    funny 404 page

--- app/pages/404.vue
+++ app/pages/404.vue
@@ -1,14 +1,36 @@
 <template>
   <main-layout>
-    <p>Page not found</p>
+    <div class="wrapper">
+      <iframe src="https://giphy.com/embed/VwoJkTfZAUBSU" width="332" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      <div class="overview">
+
+      </div>
+    </div>
   </main-layout>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
-  export default {
-    components: {
-      MainLayout
-    }
-  }
+import MainLayout from '../layouts/Main.vue';
+export default {
+  components: {
+    MainLayout,
+  },
+};
 </script>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 75%;
+  }
+  .overview {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+
+  .giphy-embed {
+    flex: 1;
+  }
+</style>
